9933a62a8868eac6ab17376d2a851ea6dabda43e
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-24 17:54:28 -0600

    Test create_directory

--- tests/generator/macro/test_base.py
+++ tests/generator/macro/test_base.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+from errno import EEXIST
 from unittest import TestCase
 
 from mock import call, MagicMock, patch
@@ -20,3 +21,38 @@ class MacroTestCase(TestCase):
 
     def construct_macro(self):
         self.macro = Macro()
+
+
+class CreateDirectoryUnitTests(MacroTestCase):
+    PATH = 'qqq'
+
+    @patch(
+        'wotw_blogger.generator.macro.base.makedirs',
+        side_effect=OSError()
+    )
+    def test_creating_error(self, mock_make):
+        mock_make.assert_not_called()
+        self.assertRaises(
+            OSError,
+            Macro.create_directory,
+            self.PATH
+        )
+
+    @patch(
+        'wotw_blogger.generator.macro.base.makedirs',
+    )
+    def test_creating_existing(self, mock_make):
+        error = OSError()
+        error.errno = EEXIST
+        mock_make.side_effect = error
+        mock_make.assert_not_called()
+        Macro.create_directory(self.PATH)
+        mock_make.assert_called_once_with(self.PATH)
+
+    @patch(
+        'wotw_blogger.generator.macro.base.makedirs'
+    )
+    def test_creating_new(self, mock_make):
+        mock_make.assert_not_called()
+        Macro.create_directory(self.PATH)
+        mock_make.assert_called_once_with(self.PATH)
