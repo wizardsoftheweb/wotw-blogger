cdfc1a1a0ae1b22c3c8009c27ffd990e9c64d76a
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-24 19:34:17 -0600

    Test ctor

--- tests/test_post.py
+++ tests/test_post.py
@@ -21,11 +21,18 @@ class PostTestCase(TestCase):
         del self.post
 
     def construct_post(self):
+        basename_patcher = patch('wotw_blogger.post.basename')
+        self.mock_basename = basename_patcher.start()
         self.post = Post(None, None, None)
+        basename_patcher.stop()
 
 
 class ConstructorUnitTests(PostTestCase):
-    """"""
+
+    def test_assignment(self):
+        self.assertIsNone(self.post.post_path)
+        self.assertIsNone(self.post.jinja_to_use)
+        self.assertIsNone(self.post.build_path)
 
 
 class InitialRenderUnitTests(PostTestCase):
