6f2435f5824665c7f717857f1b110a841086b77f
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-24 19:24:21 -0600

    Create PostTestCase

---
+++ tests/test_post.py
@@ -0,0 +1,24 @@
+# pylint: disable=W,C,R
+
+from __future__ import print_function
+
+from collections import OrderedDict
+from errno import EEXIST
+from unittest import TestCase
+
+from mock import call, MagicMock, patch
+
+from wotw_blogger import Post
+
+
+class PostTestCase(TestCase):
+
+    def setUp(self):
+        self.construct_post()
+        self.addCleanup(self.wipe_post)
+
+    def wipe_post(self):
+        del self.post
+
+    def construct_post(self):
+        self.post = Post(None, None, None)
